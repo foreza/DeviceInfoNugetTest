1730f95bc72803114073d143b036d17f1ff696d5
Jason C <jforeza@gmail.com>
2021-08-31 17:01:46 -0700

    Update Prepackage.bat

--- DeviceInfoFetcher/Prepackage.bat
+++ DeviceInfoFetcher/Prepackage.bat
@@ -1,12 +1,3 @@
-:: Pre Nuget Packaging Script
-:: 
-:: Should be ran after building DeviceInfoFetcher & InMobiSDKUtilsWinRT are built in release with x86 & x64,
-:: but before the nuget pack command is ran to finalize the Nuget Package
-::
-:: USAGE - From the VS 2019 Dev Command Prompt, enter the command: PATH\TO\PreNugetPackage.bat PATH\TO\WINDOWS\SDK\REPO
-:: EXAMPLE COMMAND: "PreNugetPackage.bat "C:\Users\Jason C\Desktop\Windows-SDK"
-
-
 @ECHO Device Info Sample Nuget Pre Packaging Batch Script
 @SET VSDevCmdPromptHome=%CD%
 @SET RepoPath=%~dp0
